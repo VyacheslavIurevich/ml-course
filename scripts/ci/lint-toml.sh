1274c3f72d99420809798e194dd60d865689c82d
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2026-06-11 05:12:10 +0300

    chores: remove non-needed file, update scripts
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- scripts/ci/lint-toml.sh
+++ scripts/ci/lint-toml.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 echo "Lint the pyproject.toml"
 echo "Run taplo lint"
-taplo lint pyproject.toml
+uv tool run taplo lint pyproject.toml
 echo "Run taplo format --check"
-taplo format --check pyproject.toml
+uv tool run taplo format --check pyproject.toml
 echo "OK"
